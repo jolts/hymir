9a1ec33d3f653254b9cbe79a7e291e67ca07ac06
Gigamo <gigamo@gmail.com>
2009-12-31 01:15:05 +0100

    Fix posts order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :authenticate, :only => [:new, :create, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all(:order => 'created_at DESC')
   end
 
   def show
