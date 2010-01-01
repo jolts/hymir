53ac81e3907c3cbd499a3a9f9fcd67ed741e721e
Gigamo <gigamo@gmail.com>
2010-01-01 23:41:56 +0100

    Add DB seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+User.create(
+  :username => 'admin',
+  :email => 'gigamo@gmail.com',
+  :password => 'abcdef',
+  :password_confirmation => 'abcdef',
+  :roles_mask => '1'
+)
+
+admin_user = User.first(:username => 'admin')
+
+Post.create(
+  :user_id => admin_user.id,
+  :title => 'Welcome to Hymir!',
+  :tags => 'welcome; hymir',
+  :body => 'Welcome to Hymir! Should be more stuff here...',
+  :created_at => Time.now,
+  :updated_at => Time.now
+)
