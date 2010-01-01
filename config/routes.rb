6b3131aa9d8fbdcfebca7da6eac2b6024edf5612
Gigamo <gigamo@gmail.com>
2010-01-02 00:20:54 +0100

    Clean up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,45 @@
 ActionController::Routing::Routes.draw do |map|
-  map.signup 'signup', :controller => 'users', :action => 'new'
-  map.login 'login', :controller => 'user_sessions', :action => 'new'
-  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'
+  # Sessions
+  map.signup 'signup', :controller => :users, :action => :new
+  map.login  'login',  :controller => :user_sessions, :action => :new
+  map.logout 'logout',
+    :controller => :user_sessions,
+    :action     => :destroy,
+    :method     => :delete
+  map.resource  :user_sessions, :only => [:create]
 
+  # Users
   map.resources :users, :only => [:new, :create]
-  map.resource :user_sessions, :only => [:new, :create, :destroy]
 
-  map.resources :posts
-  map.slug 'posts/:year/:month/:day/:slug', :controller => 'posts', :action => 'show'
-  map.tag 'tag/:tag', :controller => 'posts', :action => 'tag'
+  # Posts
+  map.resources :posts, :member => {:tag => :get} # :archive => :get
 
-  map.root :posts
+  # Find posts by slug
+  map.slug 'posts/:year/:month/:day/:slug',
+    :controller => :posts,
+    :action     => :show
+
+  # Tags
+  map.tag 'tag/:tag',
+    :controller => :posts,
+    :action => :tag
+
+  # Archives
+  # TODO: Implement this
+=begin
+  map.archive_day 'archive/:year/:month/:day',
+    :controller => :posts,
+    :action     => :archive
+  map.archive_month 'archive/:year/:month',
+    :controller => :posts,
+    :action     => :archive
+  map.archive_year 'archive/:year',
+    :controller => :posts,
+    :action     => :archive
+  map.archive 'archive',
+    :controller => :posts,
+    :action     => :archive
+=end
+
+  map.root :controller => :posts, :action => :index
 end
