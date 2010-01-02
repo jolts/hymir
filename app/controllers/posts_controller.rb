b295664176cb95d81ad0ecd17559e939d444684b
Gigamo <gigamo@gmail.com>
2010-01-02 16:49:09 +0100

    Why am I always too fast

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def tag
-    Post.paginate(:per_page => 10, :page => params[:page], :order => 'created_at DESC').select do |p|
+    @posts = Post.paginate(:per_page => 10, :page => params[:page], :order => 'created_at DESC').select do |p|
       p.tags.include?(params[:tag])
     end
 
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
   end
 
   def archive
-    Post.paginate(:per_page => 10, :page => params[:page], :order => 'created_at DESC').select do |p|
+    @posts = Post.paginate(:per_page => 10, :page => params[:page], :order => 'created_at DESC').select do |p|
       if params[:day]
         p.created_at.day   == params[:day].to_i   &&
         p.created_at.month == params[:month].to_i &&
