287f91aefe44383b7a80b07b54193b07c6855fa9
Gigamo <gigamo@gmail.com>
2010-01-04 21:25:52 +0100

    Added Hymir lib

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  include Hymir # lib/hymir.rb
   include Authentication # lib/authentication.rb
 
   helper :all # include all helpers, all the time
---
+++ lib/hymir.rb
@@ -0,0 +1,3 @@
+module Hymir
+  Version = '0.0.1 (alpha)'
+end
