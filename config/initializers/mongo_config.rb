9d00ec1c79c0ddc01843ba520c6f2761dbe7e646
Gigamo <gigamo@gmail.com>
2010-01-08 12:34:27 +0100

    Support Passenger

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,7 @@
 MongoMapper.database = "hymir-#{Rails.env}"
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    MongoMapper.connection.connect_to_master if forked
+  end
+end
