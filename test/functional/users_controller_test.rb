2095574a894de130d15d20dedced2ce173387c16
Gigamo <gigamo@gmail.com>
2010-01-10 12:36:22 +0100

    Initial users controller test

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context 'Creating a new user as a guest' do
+    # These actions are unavailable for non-admins
+    setup do
+      get :new
+    end
+
+    should_redirect_to(Hymir::Config[:title]) { root_path }
+
+    should 'have "Access denied" in flash[:error]' do
+      assert flash[:error].include?('Access denied')
+    end
+  end
+
+  context 'Listing users as a guest' do
+    setup do
+      get :index
+    end
+
+    should_redirect_to(Hymir::Config[:title]) { root_path }
+
+    should 'have "Access denied" in flash[:error]' do
+      assert flash[:error].include?('Access denied')
+    end
   end
 end
