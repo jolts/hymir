ff3cd0c5d54941d48b5efda4e6d0f5769546fda8
Gigamo <gigamo@gmail.com>
2010-01-10 13:38:13 +0100

    Add published status to posts [unfinished]

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post
   key :body, String, :required => true, :allow_blank => false
   key :slug, String, :unique => true
   key :tags, Array
+  key :published, Boolean
   timestamps!
 
   belongs_to :user
