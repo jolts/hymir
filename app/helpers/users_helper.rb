1d023bf78a723cb27e9e4776a949d173d04a367e
Gigamo <gigamo@gmail.com>
2010-01-12 22:05:35 +0100

    Tweakies

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def users_list_classes(users, user)
+    if users.first == users.last
+      cls = ' single'
+    else
+      cls = ' first' if user == users.first
+      cls = ' last'  if user == users.last
+    end
+
+    cls.blank? ? nil : cls
+  end
 end
