70ce3fa056af3d4b4279fa7890e9429f48e4dc9c
Gigamo <gigamo@gmail.com>
2010-01-13 15:46:27 +0100

    Style

--- test/factories/post.rb
+++ test/factories/post.rb
@@ -1,5 +1,5 @@
 Factory.define(:post) do |p|
-  p.user_id { |u| u.association(:user).id }
+  p.user_id {|u| u.association(:user).id}
   p.title 'Foo bar baz'
   p.body 'Hello World'
   p.tag_names 'foo; bar; baz'
