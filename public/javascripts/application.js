4c241fe1fe231dc1f7fe1dd35e065c291b7a782b
Gigamo <gigamo@gmail.com>
2010-01-13 19:19:51 +0100

    Use Cufon

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,10 +4,14 @@
   var setupHandlers = function() {
     $('a[rel*=tipsy]').tipsy({gravity: $.fn.tipsy.autoNS});
   }
+  var setupCufon = function() {
+    Cufon.replace('h1, h2, h3, h4, h5, h6, #nav, #footer');
+  }
 
   Hymir.App = {
     init: function() {
       setupHandlers();
+      setupCufon();
     }
   }
 })();
