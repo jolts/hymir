4711c1c04bc4a8e2639e8016af35cf97a2f13aa9
Gigamo <gigamo@gmail.com>
2010-01-13 14:11:51 +0100

    Fix tests

--- test/factories/post.rb
+++ test/factories/post.rb
@@ -2,5 +2,5 @@ Factory.define(:post) do |p|
   p.user_id { |u| u.association(:user).id }
   p.title 'Foo bar baz'
   p.body 'Hello World'
-  p.named_tags 'foo; bar; baz'
+  p.tag_names 'foo; bar; baz'
 end
