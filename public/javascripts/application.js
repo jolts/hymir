08588f96c7d89619a7f4b54aeb88ce0af9411495
Gigamo <gigamo@gmail.com>
2010-01-14 19:31:17 +0100

    Use cufon for pagination too

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,7 @@
     $('a[rel*=tipsy]').tipsy({gravity: $.fn.tipsy.autoNS});
   }
   var setupCufon = function() {
-    Cufon.replace('h1, h2, h3, #nav, #footer');
+    Cufon.replace('h1, h2, h3, #nav, #footer, #pagination');
   }
 
   Hymir.App = {
