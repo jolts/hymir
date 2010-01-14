1465af1c272491fdebd155b395bb6d3696157a67
Gigamo <gigamo@gmail.com>
2010-01-14 15:31:14 +0100

    Style fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,11 @@
 module UsersHelper
   def users_list_classes(users, user)
+    cls = user.id == current_user.id ? ' self' : ''
     if users.first == users.last
-      cls = ' single'
+      cls += ' single'
     else
-      cls = ' first' if user == users.first
-      cls = ' last'  if user == users.last
+      cls += ' first' if user == users.first
+      cls += ' last'  if user == users.last
     end
 
     cls.blank? ? nil : cls
