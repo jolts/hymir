6097f7b4bcfc5f8fa7e6f89061d4fb910546dcf4
Gigamo <gigamo@gmail.com>
2010-01-14 20:46:58 +0100

    Simplify users view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,2 @@
 module UsersHelper
-  def users_list_classes(users, user)
-    cls = user.id == current_user.id ? ' self' : ''
-    if users.first == users.last
-      cls += ' single'
-    else
-      cls += ' first' if user == users.first
-      cls += ' last'  if user == users.last
-    end
-
-    cls.blank? ? nil : cls
-  end
 end
