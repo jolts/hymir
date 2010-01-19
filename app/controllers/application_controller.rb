bacaeee612c5647ff55edc271a7e031639a87ef1
Gigamo <gigamo@gmail.com>
2010-01-19 19:31:23 +0100

    Filter pw confirmation from logs too

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = 'Access denied.'
