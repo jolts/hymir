6d5976bf16a9921ebfefdcf0df6e6035822bd0ff
Gigamo <gigamo@gmail.com>
2010-01-25 16:14:19 +0100

    Fixes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,24 +2,33 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    # Guests can create comments and see everything except users
     can :create, Comment
     can [:read, :archive, :tag], :all
+
     cannot :read, User
+
+    # Someone logged in
     if user
+      # Authors can create posts and edit their own posts
       if user.role?(:author)
         can :create, Post
         can :update, Post do |p|
           p.try(:user) == user
         end
       end
+
+      # Moderators can destroy comments
       if user.role?(:moderator)
-        can :update, Post
-        can :destroy, [Post, Comment]
-        can :read, User
+        can :destroy, Comment
       end
+
+      # Admins can manage everything
       if user.role?(:admin)
         can :manage, :all
       end
+
+      # Put this last so that even admins can only edit their own password
       can :update, User do |u|
         u == user
       end
