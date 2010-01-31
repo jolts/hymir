2ec5ee5221b399d468ad69f4612a86335df15b4d
Gigamo <gigamo@gmail.com>
2010-01-31 01:12:19 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-admin_user = User.create(
+admin_user = User.new(
   :username => 'admin',
   :email => Hymir::Config[:email],
   :password => 'abcdef',
@@ -16,8 +16,9 @@ admin_user = User.create(
 
 puts 'Successfully created admin user' if admin_user.save!
 
-welcome_post = Post.create(
+welcome_post = Post.new(
   :user_id => admin_user.id,
+  :creator_id => admin_user.id,
   :title => 'Welcome to Hymir!',
   :body => 'Should be more stuff here&hellip;',
   :tags => ['welcome', 'hymir'],
@@ -26,3 +27,15 @@ welcome_post = Post.create(
 )
 
 puts 'Successfully created welcome post' if welcome_post.save!
+
+about_page = Page.new(
+  :user_id => admin_user.id,
+  :creator_id => admin_user.id,
+  :title => 'About',
+  :body => 'Fill me',
+  :url => 'about',
+  :created_at => Time.now,
+  :updated_at => Time.now
+)
+
+puts 'Successfully created about page' if about_page.save!
--- doc/README.md
+++ doc/README.md
@@ -26,6 +26,8 @@ Since Hymir uses Mongo there's no need to migrate the database schema or
 anything the like.  
 You'll probably want to seed your database with an
 initial user though, so have a look in `db/seed.rb`, enter your user
-details and run `rake db:seed`.
+details and run `rake db:seed`.  
+Last but not least, you can change the site's title and some other
+things in `config/hymir.yml`.
 
 Now simply start your mongo daemon and run `script/server`.
