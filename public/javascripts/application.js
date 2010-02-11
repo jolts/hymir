f031aedd6e780b38c5f3431b4f606d7976a6fb3d
Gigamo <gigamo@gmail.com>
2010-02-11 16:25:40 +0100

    Fix js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,13 @@
 (function() {
   window.Hymir = {}
 
-  var setupHandlers = function() {
-    $('a[rel*=tipsy]').tipsy({gravity: $.fn.tipsy.autoNS});
-  }
-  var setupCufon = function() {
-    Cufon.replace('h1, h2, h3, #nav, #footer, #pagination, #locales');
-  }
-
   Hymir.App = {
     init: function() {
-      setupHandlers();
-      //setupCufon();
+      this.setupTipsy();
+    },
+
+    setupTipsy: function() {
+      $('a[rel*=tipsy]').tipsy({gravity: $.fn.tipsy.autoNS});
     }
   }
 })();
