071459737ca9a232fce71ca11c944ec0a32db058
Gigamo <gigamo@gmail.com>
2010-02-12 17:59:27 +0100

    Add some more jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,10 +4,27 @@
   Hymir.App = {
     init: function() {
       this.setupTipsy();
+      this.setupFlashFade();
+      this.setupActionHover();
     },
 
     setupTipsy: function() {
       $('a[rel*=tipsy]').tipsy({gravity: $.fn.tipsy.autoNS});
+    },
+
+    setupFlashFade: function() {
+      flash_div = $('#flash_notice, #flash_error');
+      flash_div.hide();
+      flash_div.slideDown(350);
+      flash_div.delay(5000).slideUp(700);
+    },
+
+    setupActionHover: function() {
+      $('.actions').hide();
+      $('.content, .post_comment').hover(
+        function() { $(this).find('.actions').fadeIn(100); },
+        function() { $(this).find('.actions').fadeOut(100); }
+      );
     }
   }
 })();
